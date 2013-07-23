9107f61497c70fa2b10e78d09103a09840bb22bd
Hack Reactor Students <github@hackreactor.com>
2013-07-23 16:12:18 -0700

    some stopframing functinality

--- Timeline.js
+++ Timeline.js
@@ -26,3 +26,29 @@ App.keyframes = {
   }
 };
 
+var stopFrame = function(){
+  var keys = [];
+  var frames = new Array(120);
+  for(var key in App.keyframes.reel){
+    keys.push(key);
+  }
+  keys.sort();
+  // loop through start and end keys
+  for(var i = 0; i < keys.length-1; i++){
+    var diff = {};
+    var startTime = keys[i];
+    var endTime = keys[i+1];
+    var startKeyframe = App.keyframes.reel[startTime];
+    var endKeyframe = App.keyframes.reel[endTime];
+
+
+    // push frames 
+    for(j = startTime; j < endTime; j++){
+      diff.x = (endKeyframe.x - startKeyframe.x / startTime - endTime) * j;
+      diff.y = (endKeyframe.y - startKeyframe.y / startTime - endTime) * j;
+      frames[j] = diff;
+    }
+  }
+
+  return frames;
+};
