73f99420e97a8d80d48da814a0460263826f3107
Hack Reactor Students <github@hackreactor.com>
2013-07-26 11:45:58 -0700

    Objects now not visible before created

--- EditShape.js
+++ EditShape.js
@@ -1,19 +1,25 @@
-App.canvas.on('object:modified', function(options){
+var keyframeHandler = function(options){
   var keyframes = options.target.keyframes;
 
   // Set properties
   var prop = {};
-  prop['top'] = options.target.top;
-  prop['left'] = options.target.left;
-  prop['scaleX'] = options.target.scaleX;
-  prop['scaleY'] = options.target.scaleY;
-  prop['angle'] = options.target.angle;
-  prop['height'] = options.target.height;
-  prop['width'] = options.target.width;
+  prop['top']     = options.target.top;
+  prop['left']    = options.target.left;
+  prop['scaleX']  = options.target.scaleX;
+  prop['scaleY']  = options.target.scaleY;
+  prop['angle']   = options.target.angle;
+  prop['height']  = options.target.height;
+  prop['width']   = options.target.width;
+  prop['visible'] = true;
 
   // create a new keyframe
   keyframes[App.currentFrame] = prop;
+  keyframes[6000] = prop;
+
   // update the keyframe index array
   keyframes['index'].contains(App.currentFrame) || keyframes['index'].push(App.currentFrame);
   keyframes['index'].sort(function sortNumber(a,b) {return a - b;});
-});
+};
+
+App.canvas.on('object:modified', keyframeHandler);
+App.canvas.on('object:added', keyframeHandler);
--- ExtendKlass.js
+++ ExtendKlass.js
@@ -1,6 +1,6 @@
 App.addKeys = function(klass){
   var result = klass;
-  result.keyframes = {index:[]};
+  result.keyframes = {0: {visible: false}, index:[0]};
   return result;
 };
 
@@ -9,7 +9,7 @@ fabric.Object.prototype.anim = function(t){
     var keyIndex = this.keyframes.index;
 
     var scan = (function(){
-      var result = 1;
+      var result = 0;
       while(keyIndex[result] < t){
         result++;
       }
@@ -17,14 +17,21 @@ fabric.Object.prototype.anim = function(t){
     })();
 
     var startTime = keyIndex[scan -1];
-    var endTime = keyIndex[scan];
+    var endTime = keyIndex[scan] || 0;
     var duration = endTime - startTime;
 
     var options = {};
     for(var property in keyframes[endTime]){
-      var motionPerFrame = (keyframes[endTime][property] - keyframes[startTime][property]) / duration;
-      options[property] = keyframes[startTime][property] + motionPerFrame*(t - keyIndex[scan-1]);
+      if(property === 'visible'){
+        options[property] = keyframes[startTime][property];
+      } else if (keyframes[startTime][property] && keyframes[startTime][property] !== keyframes[endTime][property]) {
+        var motionPerFrame = (keyframes[endTime][property] - keyframes[startTime][property]) / duration;
+        options[property] = keyframes[startTime][property] + motionPerFrame*(t - keyIndex[scan-1]);
+      }
     }
     this.set(options).setCoords();
     App.canvas.renderAll();
 };
+
+//should always have a 0 keyframes
+// 0 keyframe should be visible false
