f30136fa7b6cd2359564ab5d1788fc994a68a63c
Hack Reactor Students <github@hackreactor.com>
2013-07-26 15:35:47 -0700

    Removed files from kinetic

--- EditShape.js
+++ EditShape.js
@@ -3,7 +3,8 @@ var keyframeHandler = function(options){
 
   // create a new keyframe
   keyframes[App.currentFrame] = App.getProps(options.target);
-  keyframes[6000] = App.getProps(options.target);
+  //TODO: creates a bug where objects continue moving
+  //keyframes[6000] = App.getProps(options.target);
 
   // update the keyframe index array
   keyframes['index'].contains(App.currentFrame) || keyframes['index'].push(App.currentFrame);
@@ -12,3 +13,16 @@ var keyframeHandler = function(options){
 
 App.canvas.on('object:modified', keyframeHandler);
 App.canvas.on('object:added', keyframeHandler);
+
+App.getProps = function(target){
+  var prop = {};
+  prop['top']     = target.top;
+  prop['left']    = target.left;
+  prop['scaleX']  = target.scaleX;
+  prop['scaleY']  = target.scaleY;
+  prop['angle']   = target.angle;
+  prop['height']  = target.height;
+  prop['width']   = target.width;
+  prop['visible'] = true;
+  return prop;
+};
