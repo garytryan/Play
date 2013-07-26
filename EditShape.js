1d10b59d687978f8e6dab4a37770b0ae52e9d3e4
Hack Reactor Students <github@hackreactor.com>
2013-07-26 12:09:51 -0700

    refactored editShapes.js

--- EditShape.js
+++ EditShape.js
@@ -1,20 +1,9 @@
 var keyframeHandler = function(options){
   var keyframes = options.target.keyframes;
 
-  // Set properties
-  var prop = {};
-  prop['top']     = options.target.top;
-  prop['left']    = options.target.left;
-  prop['scaleX']  = options.target.scaleX;
-  prop['scaleY']  = options.target.scaleY;
-  prop['angle']   = options.target.angle;
-  prop['height']  = options.target.height;
-  prop['width']   = options.target.width;
-  prop['visible'] = true;
-
   // create a new keyframe
-  keyframes[App.currentFrame] = prop;
-  keyframes[6000] = prop;
+  keyframes[App.currentFrame] = App.getProps(options.target);
+  keyframes[6000] = App.getProps(options.target);
 
   // update the keyframe index array
   keyframes['index'].contains(App.currentFrame) || keyframes['index'].push(App.currentFrame);
--- Util.js
+++ Util.js
@@ -1,3 +1,4 @@
+// checks to see if an array holds a value
 Array.prototype.contains = function(target){
   for(i = 0; i < this.length; i++){
     if(this[i] === target){
@@ -5,4 +6,18 @@ Array.prototype.contains = function(target){
     }
   }
   return false;
+};
+
+// returns a hash of a targets useful object properties
+App.getProps = function(target){
+  var prop = {};
+  prop['top']     = target.top;
+  prop['left']    = target.left;
+  prop['scaleX']  = target.scaleX;
+  prop['scaleY']  = target.scaleY;
+  prop['angle']   = target.angle;
+  prop['height']  = target.height;
+  prop['width']   = target.width;
+  prop['visible'] = true;
+  return prop;
 };
