c1d429cd9b26d92d23f957f2f4f4898bee6c0682
Hack Reactor Students <github@hackreactor.com>
2013-07-26 15:45:37 -0700

    removed default properties from CreateShape.js

--- CreateShape.js
+++ CreateShape.js
@@ -1,34 +1,6 @@
 App.addShape = {
-  _shape: null,
-  editable: false,
-  defaultProp: {
-    top: 50,
-    left: 50,
-    height: 100,
-    width: 100,
-    fill:        'blue',
-    stroke:      'black',
-    draggable:   'true'
-  },
-
   init: function(type, properties, keyFrames){
     properties = properties || this.defaultProp;
     App.canvas.add(App.addKeys(new fabric[type](properties)));
   }
 };
-
-
-
-
-
-// var anim = new Kinetic.Animation(function(frame) {
-//   var time = frame.time,
-//       timeDiff = frame.timeDiff,
-//       frameRate = frame.frameRate;
-
-//   // update stuff
-//   var shape = shapeLayer.children;
-//   for(var i = 0; i < shapeLayer.children.length; i++){
-//     shape[i].newFrame(time);
-//   }
-// }, shapeLayer);
