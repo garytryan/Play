1a573eee5c322dec60e4eee3ba8ef549ada3a88b
Hack Reactor Students <github@hackreactor.com>
2013-07-27 17:40:58 -0700

    modified:   server.js (setup basic hello world express server)

--- server.js
+++ server.js
@@ -1,2 +1,12 @@
 var express = require('express');
 var app = express();
+
+app.get('/hello.txt', function(req, res){
+  var body = 'Hello World';
+  res.setHeader('content-Type', 'text/plain');
+  res.setHeader('content-Length', body.length);
+  res.end(body);
+});
+
+app.listen(9000);
+console.log('Listening on port 9000');
