e4ab56a117496a1ea86e5b85b47f1db1f07d9bc8
Hack Reactor Students <github@hackreactor.com>
2013-08-06 09:46:41 -0700

    added a play button to the reel view

--- app/js/templates/reel.js
+++ app/js/templates/reel.js
@@ -1,5 +1,5 @@
 define(['../libs/handlebars/handlebars'], function(handlebars){
-  var source = '<input type="range" id="range" max="6000" min="0" value="{{ currentFrame }}" step="10" />';
+  var source = '<span class="icon playBtn"></span><div id="reelWrapper"><input type="range" id="range" max="6000" min="0" value="{{ currentFrame }}" step="10" /></div>';
   var template = Handlebars.compile(source);
   return function(context){
     return template(context);
