911bc96c18e8ab4b0cfe3c22fd374ad8789c67a1
Hack Reactor Students <github@hackreactor.com>
2013-08-08 22:01:27 -0700

    initial setup for moch tests

--- test/test.js
+++ test/test.js
@@ -1,6 +1,9 @@
-var expect = requre("chai").expect;
-descibe('Array', function(){
-  descibe('#indexOf()', function(){
+if(typeof require !== 'undefined'){
+  var expect = require('chai').expect;
+}
+
+describe('Array', function(){
+  describe('#indexOf()', function(){
     it('should return -1 when the value is not present', function(){
       expect([1,2,3].indexOf(5)).to.equal(-1);
     });
