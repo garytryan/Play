d0434eda7ec891ffe4b333ad963481132acddcb5
Hack Reactor Students <github@hackreactor.com>
2013-08-09 09:59:00 -0700

    style tweeks

--- app/test/test.js
+++
@@ -1,9 +0,0 @@
-var assert = requre("assert");
-descibe('Array', function(){
-  descibe('#indexOf()', function(){
-    it('should return -1 when the value is not present', function(){
-      assert.equal(-1, [1,2,3].indexOf(5));
-      assert.equal(-1, [1,2,3].indexOf(0));
-    });
-  });
-});
